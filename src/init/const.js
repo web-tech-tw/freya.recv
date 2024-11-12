1f5a284e7dfee95fc7ae2cd13ee4b6752d9b8e33
不知火 Shiranui <supersonic@livemail.tw>
2024-11-12 15:40:15 +0800

    Initial commit

---
+++ src/init/const.js
@@ -0,0 +1,13 @@
+"use strict";
+// Constants
+
+exports.APP_NAME = "template.recv";
+exports.APP_DESCRIPTION = "A tiny but powerful microservice framework.";
+
+exports.APP_VERSION = "latest";
+
+exports.APP_AUTHOR_NAME = "Taiwan Web Technology Promotion Organization";
+exports.APP_AUTHOR_URL = "https://web-tech.tw";
+
+exports.OPENAPI_EXPORTED_FILENAME = "openapi_exported.json";
+exports.PUBLIC_KEY_FILENAME = "keypair_public.pem";
